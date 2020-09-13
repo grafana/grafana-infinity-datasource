44d1e811acf44ce09cde32e2b78eebf0f16cdfb6
Sriramajeyam <yesoreyeram@gmail.com>
2020-09-13 11:35:20 +0100

    Changed to FC

--- src/editors/FormatChooser.tsx
+++ src/editors/FormatChooser.tsx
@@ -9,31 +9,31 @@ interface FormatChooserProps {
     onChange: (value: any) => void;
 }
 
-export class FormatChooser extends React.PureComponent<FormatChooserProps> {
-    onSelectChange = (selectableItem: SelectableValue, field: string, props: any, format = 'string') => {
-        const { query, onChange } = props;
+export const FormatChooser = (props: FormatChooserProps) => {
+
+    const { query, onChange } = props;
+
+    const onSelectChange = (selectableItem: SelectableValue, field: string, format = 'string') => {
         set(query, field, format === 'number' ? toInteger(selectableItem.value) : selectableItem.value);
         onChange(query);
     }
-    render() {
-        const query = this.props.query;
-        return (
-            <div className="gf-form-inline">
-                <div className="gf-form">
-                    <label className="gf-form-label query-keyword width-8">Format</label>
-                    <Select
-                        className="min-width-12 width-12"
-                        value={
-                            SCRAP_QUERY_RESULT_FORMATS.find((field: any) => field.value === query.format)
-                            || { value: 'table', label: 'Table' }
-                        }
-                        options={SCRAP_QUERY_RESULT_FORMATS}
-                        defaultValue={{ value: 'table', label: 'Table' }}
-                        onChange={e => this.onSelectChange(e, 'format', this.props)}
-                    ></Select>
-                </div>
-            </div>
 
-        )
-    }
+    return (
+        <div className="gf-form-inline">
+            <div className="gf-form">
+                <label className="gf-form-label query-keyword width-8">Format</label>
+                <Select
+                    className="min-width-12 width-12"
+                    value={
+                        SCRAP_QUERY_RESULT_FORMATS.find((field: any) => field.value === query.format)
+                        || { value: 'table', label: 'Table' }
+                    }
+                    options={SCRAP_QUERY_RESULT_FORMATS}
+                    defaultValue={{ value: 'table', label: 'Table' }}
+                    onChange={e => onSelectChange(e, 'format')}
+                ></Select>
+            </div>
+        </div>
+
+    )
 }
