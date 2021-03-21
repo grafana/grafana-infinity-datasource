f6d55a44e887c2f713325cf0b750ea0575fd7650
Sriram <yesoreyeram@gmail.com>
2021-03-21 07:16:14 +0000

    replaceVariables update

--- src/app/InfinityQuery.ts
+++ src/app/InfinityQuery.ts
@@ -3,20 +3,21 @@ import { ScopedVars } from '@grafana/data';
 import { InfinityQuery, InfinityQuerySources, InfinityQueryType } from './../types';
 
 export const replaceVariables = (query: InfinityQuery, scopedVars: ScopedVars): InfinityQuery => {
-  query.url = getTemplateSrv().replace(query.url, scopedVars, 'glob');
-  query.data = getTemplateSrv().replace(query.data, scopedVars, 'glob');
-  if (query.url_options && query.url_options.data) {
-    query.url_options.data = getTemplateSrv().replace(query.url_options.data, scopedVars, 'glob');
-  }
-  if (query.filters && query.filters.length > 0) {
-    query.filters = query.filters.map(filter => {
+  return {
+    ...query,
+    url: getTemplateSrv().replace(query.url + '', scopedVars, 'glob'),
+    data: getTemplateSrv().replace(query.data + '', scopedVars, 'glob'),
+    url_options: {
+      ...query.url_options,
+      data: getTemplateSrv().replace(query.url_options?.data ? query.url_options.data : '', scopedVars, 'glob'),
+    },
+    filters: (query.filters ? [...query.filters] : []).map(filter => {
       filter.value = filter.value.map(val => {
         return getTemplateSrv().replace(val, scopedVars, 'glob');
       });
       return filter;
-    });
-  }
-  return query;
+    }),
+  };
 };
 
 export const IsValidInfinityQuery = (query: InfinityQuery): boolean => {
