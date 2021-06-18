7509fd57c2f8f8adac1645554c6677da4727f5d9
Sriram <yesoreyeram@gmail.com>
2021-06-18 19:26:20 +0100

    fix script

--- try/scripts/install.sh
+++ try/scripts/install.sh
@@ -19,6 +19,6 @@ docker-compose --version
 echo "starting grafana"
 cd ~/grafana-infinity-datasource
 sudo docker-compose -f ./try/docker-compose.yml pull
-sudo docker-compose -d -f ./try/docker-compose.yml up
+sudo docker-compose -f ./try/docker-compose.yml up --detach
 
 exit 0;
