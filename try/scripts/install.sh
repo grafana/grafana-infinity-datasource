413c13381e5ec94f9531d3c10714bc730bb61e16
Sriram <yesoreyeram@gmail.com>
2021-06-18 19:19:48 +0100

    fix script

--- try/scripts/install.sh
+++ try/scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "installing docker"
 sudo apt-get --yes update
 sudo apt install --yes apt-transport-https ca-certificates curl software-properties-common
@@ -8,9 +9,15 @@ sudo apt update
 apt-cache policy docker-ce
 sudo apt install --yes docker-ce
 docker --version
+
+echo "installing docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
 echo "starting grafana"
 cd ~/grafana-infinity-datasource
-sudo docker compose -d -f ./try/docker-compose.yml pull
-sudo docker compose -d -f ./try/docker-compose.yml up
+sudo docker-compose -d -f ./try/docker-compose.yml pull
+sudo docker-compose -d -f ./try/docker-compose.yml up
 
 exit 0;
