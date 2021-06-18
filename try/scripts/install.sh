37d8414eea60a77c7a28e56abb39c70ec6b1516a
Sriram <yesoreyeram@gmail.com>
2021-06-18 18:58:17 +0100

    deploy fixes

--- try/scripts/install.sh
+++ try/scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "installing docker"
 sudo apt-get --yes update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install --yes apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
