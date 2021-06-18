f47fea59698131f1fbdbdc78863dbabe35000260
Sriram <yesoreyeram@gmail.com>
2021-06-18 18:54:42 +0100

    install docker

--- try/scripts/install.sh
+++ try/scripts/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "installing grafana"
+echo "installing docker"
+sudo apt-get --yes update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install --yes docker-ce
+docker --version
 exit 0;
