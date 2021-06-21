4af22f1844be55d64e31f8dedb801d33f7e929a8
Sriram <yesoreyeram@gmail.com>
2021-06-21 10:58:03 +0100

    jest test for FormatSelector

---
+++ src/editors/query/components/FormatSelector.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FormatSelector } from './FormatSelector';
+import { render, within } from '@testing-library/react';
+import { DefaultInfinityQuery, InfinityQueryFormat } from './../../../types';
+import { Components } from './../../../selectors';
+
+const { Title: LabelTitle } = Components.QueryEditor.Format.Label;
+const { Title: DropdownPlaceholderTitle } = Components.QueryEditor.Format.Dropdown.PlaceHolder;
+
+describe('FormatSelector', () => {
+  it('renders', () => {
+    const onChange = jest.fn();
+    const query = { ...DefaultInfinityQuery };
+    const wrapper = render(<FormatSelector query={query} onChange={onChange} />);
+    expect(wrapper).not.toBeNull();
+    expect(wrapper.getByTitle(LabelTitle)).toBeInTheDocument();
+    expect(wrapper.getByTitle(DropdownPlaceholderTitle)).toBeInTheDocument();
+    expect(within(wrapper.getByTitle(DropdownPlaceholderTitle)).getByText('Table')).toBeInTheDocument();
+  });
+  it('renders timeseries format', () => {
+    const onChange = jest.fn();
+    const query = { ...DefaultInfinityQuery, format: InfinityQueryFormat.TimeSeries };
+    const wrapper = render(<FormatSelector query={query} onChange={onChange} />);
+    expect(wrapper).not.toBeNull();
+    expect(wrapper.getByTitle(LabelTitle)).toBeInTheDocument();
+    expect(wrapper.getByTitle(DropdownPlaceholderTitle)).toBeInTheDocument();
+    expect(within(wrapper.getByTitle(DropdownPlaceholderTitle)).getByText('Time Series')).toBeInTheDocument();
+  });
+});
--- src/editors/query/components/FormatSelector.tsx
+++ src/editors/query/components/FormatSelector.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Select } from '@grafana/ui';
 import { InfinityQuery, SCRAP_QUERY_RESULT_FORMATS, InfinityQueryFormat } from './../../../types';
+import { Components } from './../../../selectors';
 interface FormatSelectorProps {
   query: InfinityQuery;
   onChange: (e: InfinityQuery) => void;
@@ -9,15 +10,17 @@ export const FormatSelector = (props: FormatSelectorProps) => {
   const { query, onChange } = props;
   return (
     <>
-      <label title="Format of the query" className={`gf-form-label query-keyword width-4`}>
-        Format
+      <label title={Components.QueryEditor.Format.Label.Title} className={`gf-form-label query-keyword width-4`}>
+        {Components.QueryEditor.Format.Label.Text}
       </label>
-      <Select
-        className="min-width-8 width-8"
-        value={query.format}
-        options={SCRAP_QUERY_RESULT_FORMATS}
-        onChange={(e) => onChange({ ...query, format: e.value as InfinityQueryFormat })}
-      ></Select>
+      <div title={Components.QueryEditor.Format.Dropdown.PlaceHolder.Title}>
+        <Select
+          className="min-width-8 width-8"
+          value={query.format}
+          options={SCRAP_QUERY_RESULT_FORMATS}
+          onChange={e => onChange({ ...query, format: e.value as InfinityQueryFormat })}
+        ></Select>
+      </div>
     </>
   );
 };
