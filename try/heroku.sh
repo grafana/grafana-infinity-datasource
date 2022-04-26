566d2bf58490226540a51a39840b4da9583c1ddd
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2022-04-26 12:56:35 +0100

    try update

--- try/heroku.sh
+++ try/heroku.sh
@@ -1,4 +1,4 @@
 sed -i -e 's|<HTTP_PORT>|'$PORT'|' /etc/grafana/grafana.ini
 sed -i -e 's|<DATABASE_URL>|'$DATABASE_URL'|' /etc/grafana/grafana.ini
-sed -i -e 's|<GITHUB_CLIENT_SECRET>|'$GITHUB_CLIENT_SECRET'|' /etc/grafana/grafana.ini
+sed -i -e 's|<GTM_TAG_MANAGER_ID>|'$GTM_TAG_MANAGER_ID'|' /etc/grafana/grafana.ini
 /run.sh
