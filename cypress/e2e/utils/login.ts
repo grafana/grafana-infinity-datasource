e500706c622cf124677f0d3a6ee643214680a54b
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2022-11-01 09:10:35 +0000

    fix e2e test

--- cypress/e2e/utils/login.ts
+++ cypress/e2e/utils/login.ts
@@ -2,7 +2,7 @@ const timeout = 30 * 1000;
 export const login = () => {
   cy.viewport(1792, 1017);
   cy.visit('/login', { timeout });
-  cy.contains('Welcome to Grafana Enterprise', { timeout });
+  cy.contains('Welcome to Grafana', { timeout });
   cy.get(`[aria-label="${'Username input field'}"]`, { timeout }).should('be.visible').type('infinity');
   cy.get(`[aria-label="${'Password input field'}"]`, { timeout }).should('be.visible').type('infinity');
   cy.get(`[aria-label="${'Login button'}"]`, { timeout }).click();
