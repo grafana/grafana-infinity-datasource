6b7f14480761cdf5f618424ef170595ffcba59fd
Sriramajeyam Sugumaran <153843+yesoreyeram@users.noreply.github.com>
2023-02-01 06:56:33 +0000

    update jest snapshots

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
-// force timezone to UTC to allow tests to work regardless of local timezone
-// generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
+const { grafanaESModules, nodeModulesToTransform } = require('./.config/jest/utils');
 
 module.exports = {
-  // Jest configuration provided by Grafana scaffolding
+  // Jest configuration provided by @grafana/create-plugin
   ...require('./.config/jest.config'),
+  // Inform jest to only transform specific node_module packages.
+  transformIgnorePatterns: [nodeModulesToTransform([...grafanaESModules, 'rxjs', 'jsonpath-plus'])],
 };
