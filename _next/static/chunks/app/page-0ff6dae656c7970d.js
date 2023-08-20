11859bef5bd301c784d198e1c566cbfda631125d
yesoreyeram <yesoreyeram@users.noreply.github.com>
2023-08-20 03:33:29 +0000

    deploy: 74f27dd6da4b8c2c1f3b8e1cc0264ececbb0bfe6

---
+++ _next/static/chunks/app/page-0ff6dae656c7970d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{2523:function(e,n,s){Promise.resolve().then(s.t.bind(s,3222,23)),Promise.resolve().then(s.t.bind(s,6685,23))}},function(e){e.O(0,[965,685,971,596,744],function(){return e(e.s=2523)}),_N_E=e.O()}]);
