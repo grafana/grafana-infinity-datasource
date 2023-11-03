40d5624029f0c8b0302d58229db6aa7e3f8673ad
Daniel Titch <dantitch@gmail.com>
2023-11-03 10:07:18 -0400

    fixed linting errors

--- src/editors/query/query.url.tsx
+++ src/editors/query/query.url.tsx
@@ -78,8 +78,9 @@ export const URL = ({ query, onChange, onRunQuery, onShowUrlOptions }: { query: 
       <Input
         type="text"
         value={url}
-        placeholder={`https://github.com/yesoreyeram/grafana-infinity-datasource/blob/main/testdata/users.${query.type === 'graphql' || query.type === 'uql' || query.type === 'groq' ? 'json' : query.type
-          }`}
+        placeholder={`https://github.com/yesoreyeram/grafana-infinity-datasource/blob/main/testdata/users.${
+          query.type === 'graphql' || query.type === 'uql' || query.type === 'groq' ? 'json' : query.type
+        }`}
         width={84}
         onChange={(e) => setURL(e.currentTarget.value)}
         onBlur={onURLChange}
@@ -184,43 +185,44 @@ const Body = ({ query, onChange, onRunQuery }: { query: InfinityQuery; onChange:
                   />
                 </EditorField>
                 <br />
-                <EditorField label='GraphQL Variables' tooltip={placeholderGraphQLVariables}>
+                <EditorField label="GraphQL Variables" tooltip={placeholderGraphQLVariables}>
                   <CodeEditor
-                    language='json'
+                    language="json"
                     height={'200px'}
                     value={query.url_options?.body_graphql_variables || placeholderGraphQLVariables}
                     onSave={(e) => onURLOptionsChange('body_graphql_variables', e)}
                     onBlur={(e) => onURLOptionsChange('body_graphql_variables', e)}
                     onEditorDidMount={(editor, monaco) => {
-                      monaco.editor.defineTheme("graphqlVariableEditorTheme", {
-                        base: "vs-dark",
+                      monaco.editor.defineTheme('graphqlVariableEditorTheme', {
+                        base: 'vs-dark',
                         inherit: true,
-                        rules: [
-                          { token: "grafanaVariable", foreground: "#ff9922", fontStyle: ' italic' }
-                        ],
-                        colors: {}
+                        rules: [{ token: 'grafanaVariable', foreground: '#ff9922', fontStyle: ' italic' }],
+                        colors: {},
                       });
-                      monaco.editor.setTheme("graphqlVariableEditorTheme");
-                      monaco.languages.setMonarchTokensProvider("json", {
+                      monaco.editor.setTheme('graphqlVariableEditorTheme');
+                      monaco.languages.setMonarchTokensProvider('json', {
                         tokenizer: {
                           root: [
-                            [/@?[a-zA-Z][\w$]*/, {
-                              cases: {
-                                '@default': "string"
-                              }
-                            }],
+                            [
+                              /@?[a-zA-Z][\w$]*/,
+                              {
+                                cases: {
+                                  '@default': 'string',
+                                },
+                              },
+                            ],
                             [/[{}()\[\]]/, '@brackets'],
-                            [/".*?":/, "variable"],
-                            [/["|']?\${.*}["|']?/, "grafanaVariable"],
-                            [/\d*\.|d+([eE][\-+]?\d+)?/, "number.float"],
-                            [/0[xX][0-9a-fA-F]+/,"number.hex"],
-                            [/\d+/,"number"]
+                            [/".*?":/, 'variable'],
+                            [/["|']?\${.*}["|']?/, 'grafanaVariable'],
+                            [/\d*\.|d+([eE][\-+]?\d+)?/, 'number.float'],
+                            [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+                            [/\d+/, 'number'],
                           ],
-                        }
+                        },
                       });
                       monaco.languages.json.jsonDefaults.setModeConfiguration({
-                        colors:true
-                      })
+                        colors: true,
+                      });
                     }}
                   />
                 </EditorField>
@@ -252,14 +254,14 @@ const Body = ({ query, onChange, onRunQuery }: { query: InfinityQuery; onChange:
                     query.url_options?.body_content_type === 'application/json'
                       ? 'json'
                       : query.url_options?.body_content_type === 'application/xml'
-                        ? 'xml'
-                        : query.url_options?.body_content_type === 'text/html'
-                          ? 'html'
-                          : query.url_options?.body_content_type === 'application/javascript'
-                            ? 'javascript'
-                            : query.url_options?.body_content_type === 'text/plain'
-                              ? 'text'
-                              : 'text'
+                      ? 'xml'
+                      : query.url_options?.body_content_type === 'text/html'
+                      ? 'html'
+                      : query.url_options?.body_content_type === 'application/javascript'
+                      ? 'javascript'
+                      : query.url_options?.body_content_type === 'text/plain'
+                      ? 'text'
+                      : 'text'
                   }
                   height={'200px'}
                   value={query.url_options?.data || ''}
