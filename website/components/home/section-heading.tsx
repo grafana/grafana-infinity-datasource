ee11af0d4cb7e40a3bea7bc6251a89110fa6b9d3
Sriramajeyam Sugumaran <153843+yesoreyeram@users.noreply.github.com>
2024-01-02 16:56:33 +0000

    removed old website setup

--- website/components/home/section-heading.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function SectionHeading({ children }: { children: React.ReactNode }) {
-  return <h6 className="w-full text-center mb-10 text-2xl text-white font-semibold">{children}</h6>;
-}
