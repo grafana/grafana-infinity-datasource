33187286f8a1387439f6035d13865a4c04a8fe94
yesoreyeram <153843+yesoreyeram@users.noreply.github.com>
2025-01-16 00:37:21 +0000

    Fix typo

--- src/editors/config/URL.tsx
+++ src/editors/config/URL.tsx
@@ -34,16 +34,14 @@ export const URLMiscEditor = (props: DataSourcePluginOptionsEditorProps<Infinity
       <Stack>
         <InlineLabel
           width={36}
-          tooltip={`By default infinity only allow GET/POST HTTP methods to reduce the risk of destructive/accidental payloads. If you need non GET/POST methods, make use of this setting with caution. Note: Infinity dont't evaluate any permissions against the underlying API`}
+          tooltip={`By default infinity only allow GET/POST HTTP methods to reduce the risk of destructive/accidental payloads. If you need non GET/POST methods, make use of this setting with caution. Note: Infinity don't evaluate any permissions against the underlying API`}
         >
           Allow non GET / POST HTTP verbs
         </InlineLabel>
         <InlineSwitch value={jsonData.allowNonGetPostMethods || false} onChange={(e) => onOptionsChange({ ...options, jsonData: { ...jsonData, allowNonGetPostMethods: e.currentTarget.checked } })} />
       </Stack>
       <Stack>
-        <InlineLabel width={36} tooltip={'Experimantal'}>
-          Encode query parameters with %20
-        </InlineLabel>
+        <InlineLabel width={36}>Encode query parameters with %20</InlineLabel>
         <InlineSwitch value={jsonData.pathEncodedUrlsEnabled || false} onChange={(e) => onOptionsChange({ ...options, jsonData: { ...jsonData, pathEncodedUrlsEnabled: e.currentTarget.checked } })} />
         <Badge text="Experimental" color="orange" icon={'exclamation-triangle'} />
       </Stack>
