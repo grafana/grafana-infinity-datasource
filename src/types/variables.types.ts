69fe472d2eafc61ba47ae9038535e2cb97b2b5f2
Sriram <153843+yesoreyeram@users.noreply.github.com>
2026-01-07 16:03:46 +0000

    migrate variable queries to send standard `DataFrame` instead of `MetricFindValue[]` (#1407)

--- src/types/variables.types.ts
+++ src/types/variables.types.ts
@@ -1,9 +1,10 @@
 import type { InfinityQuery } from '@/types';
 
 //#region Variable Query
+export type VariableMeta = { valueField?: string; textField?: string };
 export type VariableTokenLegacy = 'Collection' | 'CollectionLookup' | 'Random' | 'Join' | 'UnixTimeStamp';
 export type VariableQueryType = 'legacy' | 'infinity' | 'random';
-export type VariableQueryBase<T extends VariableQueryType> = { queryType: T; refId: string };
+export type VariableQueryBase<T extends VariableQueryType> = { queryType: T; refId: string; meta?: VariableMeta };
 export type VariableQueryLegacy = { query: string; infinityQuery?: InfinityQuery } & VariableQueryBase<'legacy'>;
 export type VariableQueryInfinity = { infinityQuery: InfinityQuery; query?: string } & VariableQueryBase<'infinity'>;
 export type VariableQueryRandom = { values: string[] } & VariableQueryBase<'random'>;
