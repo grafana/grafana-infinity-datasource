ac99d2d0d316d2d05cad523de4162736d8bb6b8d
yesoreyeram <153843+yesoreyeram@users.noreply.github.com>
2026-02-13 12:37:32 +0000

    fix e2e tests for grafana 11

--- tests/e2e/configeditor.spec.ts
+++ tests/e2e/configeditor.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
 test('Config editor: should be able to add a global query', async ({ createDataSourceConfigPage, page }) => {
+    const version = await page.evaluate(() => window.grafanaBootData.settings.buildInfo.version);
     await createDataSourceConfigPage({ type: 'yesoreyeram-infinity-datasource' });
     await page.getByRole('button', { name: 'Global queries' }).click();
     await page.getByRole('button', { name: 'Add Global Query' }).click();
     await page.getByRole('button', { name: 'Edit' }).click();
-
-    const sourceDropdown = await page.getByTestId('infinity-query-source-selector');
+    const sourceDropdownWrapper = await page.getByTestId('infinity-query-field-wrapper-source');
+    const sourceDropdown = await sourceDropdownWrapper.getByRole('combobox');
     await sourceDropdown.click();
     await sourceDropdown.fill('Reference');
     await sourceDropdown.press('ArrowDown');
