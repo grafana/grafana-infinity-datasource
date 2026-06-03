c561f3a125f12bc326c20f578d458d7e483b865c
yesoreyeram <153843+yesoreyeram@users.noreply.github.com>
2026-06-03 18:32:16 +0100

    fix e2e smoke test

--- tests/e2e/smoke.spec.ts
+++ tests/e2e/smoke.spec.ts
@@ -2,7 +2,5 @@ import { test, expect } from '@grafana/plugin-e2e';
 
 test('Smoke test: plugin loads', async ({ createDataSourceConfigPage, page }) => {
   await createDataSourceConfigPage({ type: 'yesoreyeram-infinity-datasource' });
-
-  await expect(await page.getByText('Type: Infinity', { exact: true })).toBeVisible();
   await expect(await page.getByRole('heading', { name: '👋 Welcome to Grafana Infinity Data Source!', exact: true })).toBeVisible();
 });
