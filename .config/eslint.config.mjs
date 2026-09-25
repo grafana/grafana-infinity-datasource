c671147a8313b539e72006a72e153be0eaceef25
ismail simsek <ismailsimsek09@gmail.com>
2026-09-25 16:34:35 +0200

    Upgrade eslint to v10

--- .config/eslint.config.mjs
+++ .config/eslint.config.mjs
@@ -4,6 +4,12 @@ import grafanaConfig from '@grafana/eslint-config';
 export default defineConfig([
   ...grafanaConfig,
   {
+    // eslint-plugin-react's "detect" calls context.getFilename(), which ESLint 10 removed
+    settings: {
+      react: {
+        version: '18.3',
+      },
+    },
     rules: {
       'react/prop-types': 'off',
     },
